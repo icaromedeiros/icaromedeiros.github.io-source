277baa1e1cda17304002711f79d74e20f07d707b
Icaro Medeiros <icaro.medeiros@gmail.com>
2014-09-08 11:11:21 -0300

    added google webmaster tools

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 BRANCH=master
 TARGET_REPO=icaromedeiros/icaromedeiros.github.io.git
 PELICAN_OUTPUT_FOLDER=output
+GOOGLE_WEBMASTER_HTML=googlec782d69d9cd6cf7f.html
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Compiling as make publish"
@@ -13,6 +14,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     fi
     #using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    # google webmaster tools
+    cp $GOOGLE_WEBMASTER_HTML built_website
     #go into directory and copy data we're interested in to that directory
     cd built_website
     rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
